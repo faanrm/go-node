8e7e2685506dba9e250dd92472aec6b3501fd433
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-02-17 01:47:26 +0300

    clean repo generated ok

--- firstApp/README.md
+++
@@ -1,2 +0,0 @@
-# NodeJs-Sequelize-Starter
-A backend template using Nodejs &amp; Sequelize 
--- firstApp/src/routes/AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { loginUser } from "../controllers/AuthController.js"
-
-const router = express.Router()
-
-router.post('/login',loginUser)
-
-export default router
