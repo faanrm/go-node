54e4029cc3b36ad7ba3bc6671a73e0487e4518d8
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-02-17 13:42:30 +0300

    modify root and add flags to see version

--- utils/root.go
+++ utils/root.go
@@ -1,28 +1,46 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+var blue = "\033[34m"
+var reset = "\033[0m"
+
+var (
+	versionFlag bool
+	version     = "0.1.1"
+)
+
 var rootCmd = &cobra.Command{
 	Use: "gno",
-	Short: `
+	Short: blue + `
 _____/\\\\\\\\\\\\__/\\\\\_____/\\\_______/\\\\\______        
- ___/\\\//////////__\/\\\\\\___\/\\\_____/\\\///\\\____       
-  __/\\\_____________\/\\\/\\\__\/\\\___/\\\/__\///\\\__      
-   _\/\\\____/\\\\\\\_\/\\\//\\\_\/\\\__/\\\______\//\\\_     
-    _\/\\\___\/////\\\_\/\\\\//\\\\/\\\_\/\\\_______\/\\\_    
-     _\/\\\_______\/\\\_\/\\\_\//\\\/\\\_\//\\\______/\\\__   
-      _\/\\\_______\/\\\_\/\\\__\//\\\\\\__\///\\\__/\\\____  
-       _\//\\\\\\\\\\\\/__\/\\\___\//\\\\\____\///\\\\\/_____ 
-        __\////////////____\///_____\/////_______\/////_______
-		`,
+___/\\\//////////__\/\\\\\\___\/\\\_____/\\\///\\\____       
+__/\\\_____________\/\\\/\\\__\/\\\___/\\\/__\///\\\__      
+_\/\\\____/\\\\\\\_\/\\\//\\\_\/\\\__/\\\______\//\\\_     
+_\/\\\___\/////\\\_\/\\\\//\\\\/\\\_\/\\\_______\/\\\_    
+_\/\\\_______\/\\\_\/\\\_\//\\\/\\\_\//\\\______/\\\__   
+_\/\\\_______\/\\\_\/\\\__\//\\\\\\__\///\\\__/\\\____  
+_\//\\\\\\\\\\\\/__\/\\\___\//\\\\\____\///\\\\\/_____ 
+__\////////////____\///_____\/////_______\/////_______
+` + reset,
+	Run: func(cmd *cobra.Command, args []string) {
+		if versionFlag {
+			fmt.Println("Version:", version)
+			return
+		}
+		err := cmd.Help()
+		if err != nil {
+			fmt.Println("Error displaying help:", err)
+			os.Exit(1)
+		}
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -31,5 +49,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags()
+	rootCmd.Flags().BoolVarP(&versionFlag, "version", "v", false, "Print the version of the CLI")
 }
